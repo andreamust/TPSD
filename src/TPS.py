cd9f39d5f44ecc175220779a73e8869d1a673a02
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-03-20 14:09:47 +0100

    improved TPS visualisation

--- src/TPS.py
+++ src/TPS.py
@@ -9,21 +9,6 @@ from tabulate import tabulate
 class TPS:
     chromatic_level = list(range(0, 12))
 
-    note_map = [
-        ('C', 'Dbb', 'B#'),
-        ('C#', 'Db'),
-        ('D', 'C##', 'Ebb'),
-        ('Eb', 'D#'),
-        ('E', 'D##', 'Fb'),
-        ('F', 'E#', 'Gbb'),
-        ('F#', 'Gb'),
-        ('G', 'F##', 'Abb'),
-        ('G#', 'Ab'),
-        ('A', 'G##', 'Bbb'),
-        ('Bb', 'A#'),
-        ('B', 'A##', 'Cb')
-    ]
-
     def __init__(self, key: str, chord: list[str], scale_grades: list[str] = None, show: bool = None):
         if scale_grades is None:
             self.scale_grades = [2, 2, 1, 2, 2, 2, 1]
@@ -31,14 +16,29 @@ class TPS:
         self.chord = chord
         self.show = show
 
-    def note_index(self, note) -> int:
+    @staticmethod
+    def note_index(note) -> int:
         """
         Gets a note label and returns its index within the chromatic scale.
         :return: the index of the note within the chromatic scale.
         """
+        note_map = [
+            ('C', 'Dbb', 'B#'),
+            ('C#', 'Db'),
+            ('D', 'C##', 'Ebb'),
+            ('Eb', 'D#'),
+            ('E', 'D##', 'Fb'),
+            ('F', 'E#', 'Gbb'),
+            ('F#', 'Gb'),
+            ('G', 'F##', 'Abb'),
+            ('G#', 'Ab'),
+            ('A', 'G##', 'Bbb'),
+            ('Bb', 'A#'),
+            ('B', 'A##', 'Cb')
+        ]
 
         try:
-            return [i for i, n in enumerate(self.note_map) if note in n][0]
+            return [i for i, n in enumerate(note_map) if note in n][0]
         except IndexError:
             raise NameError('The note is not indexed, try with enharmonics.')
 
@@ -54,8 +54,6 @@ class TPS:
             if note_index >= 12:
                 note_index = note_index - 12
             diatonic_grades.append(note_index)
-        if self.show:
-            return [x if x in diatonic_grades else '_' for x in self.chromatic_level]
         return diatonic_grades
 
     def triadic_level(self):
@@ -69,8 +67,6 @@ class TPS:
         for note in self.chord:
             chord_note_index = self.note_index(note)
             triadic_grades.append(chord_note_index)
-        if self.show:
-            return [y if y in triadic_grades else '_' for y in self.chromatic_level]
         return triadic_grades
 
     def root_level(self):
@@ -79,8 +75,6 @@ class TPS:
         :return: a list of the grades belonging to the root level of the TPS.
         """
         root = self.note_index(self.chord[0])
-        if self.show:
-            return [y if y == root else '_' for y in self.chromatic_level]
         return [root]
 
     def fifth_level(self):
@@ -91,8 +85,6 @@ class TPS:
         fifth = self.note_index(self.chord[0]) + 7 if self.note_index(self.chord[0]) + 7 < 12 else self.note_index(
             self.chord[0]) + 7 - 12
         fifth_grades = [self.note_index(self.chord[0]), fifth]
-        if self.show:
-            return [y if y in fifth_grades else '_' for y in self.chromatic_level]
         return fifth_grades
 
     def show_table(self):
@@ -100,8 +92,11 @@ class TPS:
         Prints a table of the TPS if the show parameter is set to True when initialising the class.
         """
         if self.show:
-            print(tabulate([self.root_level(), self.fifth_level(), self.triadic_level(), self.diatonic_level(),
-                            self.chromatic_level, [n[0] for n in self.note_map]]))
+            show_levels = [self.chromatic_level, ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']]
+            for level in [self.root_level(), self.fifth_level(), self.triadic_level(), self.diatonic_level()][::-1]:
+                show_levels.insert(0, [grade if grade in level else '_' for grade in self.chromatic_level])
+
+            print(tabulate(show_levels))
         else:
             pass
 
